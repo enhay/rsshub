1e3c77af2dbf58722b0e30ad5cdd0e336dd71065
xuyang1 <xuyang1@huya.com>
2019-04-12 15:49:02 +0800

    + wechat

---
+++ script/run_wechat.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const _ = require('lodash');
+const wechat = require('./wechat');
+const logger = require('../lib/utils/logger');
+
+const readFile = util.promisify(fs.readFile);
+
+(async () => {
+    let sList = '';
+    try {
+        sList = await readFile(path.resolve(__dirname, '../html/wd_list.txt'), 'utf-8');
+    } catch (err) {
+        logger.error(`run_wechat.js no_wd_list ${err.stack}`);
+        return;
+    }
+    sList = _.trim(sList);
+    const originList = _.uniq(sList.split(','));
+    const list = _.chunk(originList, parseInt(originList.length / 24, 10) + 1);
+    const hours = new Date().getHours();
+    const wds = list[hours];
+    logger.info(`run_wechat.js hours=${hours} ${JSON.stringify(wds)}`);
+    if (wds) {
+        for (let i = 0; i < wds.length; i++) {
+            if (wds[i]) {
+                try {
+                    /* eslint-disable no-await-in-loop */
+                    await wechat(wds[i]);
+                } catch (err) {
+                    logger.error(`run_wechat.js ${err.stack}`);
+                }
+            }
+        }
+    }
+})();
---
+++ script/wechat.js
@@ -0,0 +1,172 @@
+/*eslint-disable*/
+const cheerio = require('cheerio');
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const readline = require('readline');
+const _ = require('lodash');
+const puppeteer = require('../lib/utils/puppeteer');
+const axios = require('../lib/utils/axios');
+const md5 = require('../lib/utils/md5');
+const logger = require('../lib/utils/logger');
+
+const access = util.promisify(fs.access);
+const writeFile = util.promisify(fs.writeFile);
+const readdir = util.promisify(fs.readdir);
+const mkdir = util.promisify(fs.mkdir);
+
+// 获取本机ip地址
+function getIPAdress() {
+    const interfaces = require('os').networkInterfaces();
+    for (const devName in interfaces) {
+        const iface = interfaces[devName];
+        for (let i = 0; i < iface.length; i++) {
+            const alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+}
+
+const ip = getIPAdress();
+
+const checkDir = async (wdPath) => {
+    try {
+        await access(wdPath);
+    } catch (err) {
+        await mkdir(wdPath, {
+            recursive: true,
+        });
+    }
+};
+
+const genHtml = async (wd, urls) => {
+    if (!urls || !urls.length || !wd) {
+        return;
+    }
+    const wdPath = path.resolve('../html', wd);
+    await checkDir(wdPath);
+    const fileNames = await readdir(wdPath);
+    const domain = 'https://mp.weixin.qq.com';
+    let pros = [];
+    pros = urls.map((item) =>
+        (async () => {
+            const htmlName = item.md5Title + '.html';
+            if (fileNames.includes(htmlName)) {
+                return;
+            }
+            try {
+                let { data } = await axios({
+                    method: 'get',
+                    url: domain + item.hrefs,
+                });
+                data = data.replace(/https?\:\/\/mmbiz\.qpic\.cn\//g, '/');
+                await writeFile(path.resolve(wdPath, item.md5Title + '.html'), data, {
+                    flag: 'w',
+                });
+            } catch (err) {
+                logger.error(`wechat.js ${err.stack}`);
+            }
+        })()
+    );
+    await Promise.all(pros);
+};
+
+const genBill = async (wd, urls, cnTitle) => {
+    if (!wd || !urls) {
+        return;
+    }
+    const centent = {
+        name: cnTitle,
+        item: [],
+    };
+    urls.forEach((i) => {
+        centent.item.push({
+            title: _.trim(i.title),
+            description: _.trim(i.title),
+            link: `http://${ip}:8080/${wd}/${i.md5Title}.html`,
+        });
+    });
+    const wdPath = path.resolve('../html', wd);
+    await writeFile(path.resolve(wdPath, 'bill.json'), JSON.stringify(centent), {
+        flag: 'w',
+    });
+};
+
+const fn = async (wd) => {
+    const link = 'https://weixin.sogou.com/';
+    const browser = await puppeteer();
+    const page = await browser.newPage();
+    await page.goto(link, {
+        waitUntil: 'domcontentloaded',
+    });
+    await page.type('#query', wd);
+    await Promise.all([
+        page.waitForNavigation({
+            waitUntil: 'load',
+        }),
+        page.click('#searchForm > div > input.swz2'),
+    ]);
+    const cnTitle = await page.evaluate(() => {
+        // 强制在当前tab里跳转，否则还需要切换tab
+        document.querySelector('#sogou_vr_11002301_box_0 > div > div.txt-box > p.tit > a').target = '';
+        return document.querySelector('#sogou_vr_11002301_box_0 > div > div.txt-box > p.tit > a').textContent;
+    });
+    // eslint-disable-next-line no-undef
+    await Promise.all([
+        page.waitForNavigation({
+            waitUntil: 'load',
+        }),
+        page.click('#sogou_vr_11002301_box_0 > div > div.txt-box > p.tit > a'),
+    ]);
+    const html = await page.evaluate(() => document.querySelector('body').innerHTML);
+    let $ = cheerio.load(html);
+    if (!$('#history').length) {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        const screenshotPath = path.resolve(__dirname, '../html/screenshot_');
+        const screenshotFile = `${wd}_${Date.now()}st.png`;
+        await page.screenshot({
+            path: path.resolve(screenshotPath, screenshotFile),
+            fullPage: true,
+        });
+        console.log('验证码:', `http://${ip}:8080/screenshot_/${screenshotFile}`);
+        logger.info(`验证码: http://${ip}:8080/screenshot_/${screenshotFile}`);
+        const as = await new Promise((r) => {
+            rl.question('请输入验证码:', (answer) => {
+                rl.close();
+                r(answer);
+            });
+        });
+        await page.type('#input', as);
+        await Promise.all([
+            page.waitForNavigation({
+                waitUntil: 'load',
+            }),
+            page.click('#bt'),
+        ]);
+        const html1 = await page.evaluate(() => document.querySelector('body').innerHTML);
+        $ = cheerio.load(html1);
+    }
+    browser.close();
+    const urls = [];
+    $('.weui_media_title').each((index, ele) => {
+        const $ele = $(ele);
+        const title = $ele.text();
+        const md5Title = md5(title);
+        const href = $ele.attr('hrefs');
+        urls.push({
+            hrefs: href,
+            md5Title,
+            title,
+        });
+    });
+    await genHtml(wd, urls);
+    await genBill(wd, urls, cnTitle);
+    console.log('success');
+};
+
+module.exports = fn;
